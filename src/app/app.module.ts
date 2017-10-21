7bfac3121bdd6f8ca9fff49b514ac852c8924f18
César Mendonça <cesar.rew@gmail.com>
2017-10-21 16:01:25 -0200

    Application update.

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { CurrencyMaskModule } from "ng2-currency-mask";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./component/app.component";
+import { CurrencyMaskDirective } from "./directive/currency-mask.directive";
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        //CurrencyMaskModule,
+        FormsModule,
+        HttpModule
+    ],
+    declarations: [
+        AppComponent,
+        CurrencyMaskDirective
+    ],
+    providers: [
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ src/resources.ts
@@ -0,0 +1,9 @@
+//Modules
+import "@angular/common";
+import "@angular/core";
+import "@angular/forms";
+import "@angular/http";
+import "@angular/platform-browser";
+import "@angular/router";
+import "ng2-currency-mask";
+import "rxjs/Rx";
