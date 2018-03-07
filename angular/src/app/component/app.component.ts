920454d99a16c834ee596a92d354ae6571cc94d5
César Mendonça <cesar.rew@gmail.com>
2018-03-07 18:35:20 -0300

    Application update.

--- angular/src/app/component/app.component.ts
+++ angular/src/app/component/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: "app",
+    selector: "app-root",
     templateUrl: "./app.component.html"
 })
 export class AppComponent {
 
-    value: number;
+    value: number = 300.23
 }
--- angular/src/resources.ts
+++
@@ -1,7 +0,0 @@
-//Modules
-import "@angular/common";
-import "@angular/core";
-import "@angular/forms";
-import "@angular/platform-browser";
-import "ng2-currency-mask";
-import "rxjs/Rx";
